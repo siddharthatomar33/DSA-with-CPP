354ab7effe494dbf1ed15195272e16e4ab0e63b7
siddhartha tomar <siddharthatomar33@gmail.com>
2025-01-07 16:21:17 +0530

    learn cpp

---
+++ praccodechef6.cpp
@@ -0,0 +1,18 @@
+//Chef classifies a day to be either rainy, cloudy, or clear.
+// In a particular week, Chef finds 
+// X
+// X days to be rainy and 
+// Y
+// Y days to be cloudy.
+// Find the number of clear days in the week.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x,y;
+    cin>>x>>y;
+    cout<<7-x-y;
+    return 0;
+}
