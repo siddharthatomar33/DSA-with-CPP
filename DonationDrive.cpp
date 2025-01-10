c590d6152dde8ce549ec062e4020d6df79a59237
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-10 21:27:29 +0530

    cpp codechef

---
+++ DonationDrive.cpp
@@ -0,0 +1,18 @@
+// Donation Drive
+// A blood drive aims to collect N number of blood donations.
+// The drive has collected X donations so far. Find the remaining number of donations needed to reach the target.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    cout<<n-x<<endl;
+	}
+
+}
