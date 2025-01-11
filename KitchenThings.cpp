c1ad61df47e22290b1e9afaa86e7647d7439c79e
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-11 23:58:51 +0530

    Codechef cpp

---
+++ KitchenThings.cpp
@@ -0,0 +1,17 @@
+// Kitchen Timings
+// The working hours of Chef’s kitchen are from 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        cout<<y-x<<endl;
+    }
+}
+
