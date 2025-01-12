9afc9a783f3301a23562fca62f76870b54012e1c
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-12 15:59:33 +0530

    codechef cpp

--- RollerCoster.cpp
+++ RollerCoster.cpp
@@ -1,7 +1,5 @@
-Roller Coaster
-Chef's son wants to go on a roller coaster ride. The height of Chef's son is 
-X
-X inches while the minimum height required to go on the ride is H inches. Determine whether he can go on the ride or not.
+// Roller Coaster
+// Chef's son wants to go on a roller coaster ride. The height of Chef's son is X inches while the minimum height required to go on the ride is H inches. Determine whether he can go on the ride or not.
 
 #include <bits/stdc++.h>
 using namespace std;
