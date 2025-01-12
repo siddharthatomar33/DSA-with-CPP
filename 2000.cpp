d949bcce308538fcff0b64b3e43765ddca623752
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-12 15:44:39 +0530

    codechef cpp

---
+++ 2000.cpp
@@ -0,0 +1,12 @@
+// 2000
+// Chef had collected N notes of Rs. 2000 to pay his total college fees. However, the government banned Rs. 2000 notes.Chef wants to pay the same amount using Rs. 500 notes only. Find the number of notes Chef needs.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n;
+    cin>>n;
+    cout<<n*4<<endl;
+}
