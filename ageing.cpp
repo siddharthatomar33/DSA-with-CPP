684e4fd7e8a008b49362004a2741174cde3a93f2
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-13 23:39:58 +0530

    codechef

---
+++ ageing.cpp
@@ -0,0 +1,22 @@
+// Ageing
+// Chef's current age is 20 years, while Chefina's current age is 10 years.
+// Determine Chefina's age when Chef will be X years old.
+// Note: Assume that Chef and Chefina were born on same day and same month (just different year).
+// Input Format
+// The first line of input will contain a single integer T, denoting the number of test cases.
+// Each test case consists of a single integer X, the age of Chef.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	 int t;
+	 cin>>t;
+	 while(t--){
+	     int x;
+	     cin>>x;
+	     cout<<x-10<<endl;
+	 }
+
+}
