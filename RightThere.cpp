9868cdb1ae1fbb0c4cfc75f96b3cb3ba37346c5f
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-13 23:48:51 +0530

    codechef

---
+++ RightThere.cpp
@@ -0,0 +1,22 @@
+// Right There
+// If you wanna party, if you, if you wanna party Then put your hands up Chef wants to host a party with a total of N people. However, the party hall has a capacity of X people. Find whether Chef can host the party.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    if(n>x){
+	        cout<<"no"<<endl;
+	    }
+	    else{
+	        cout<<"yes"<<endl;
+	    }
+	}
+
+}
