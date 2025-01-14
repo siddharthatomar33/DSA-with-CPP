5ae76789ca9590109b0097aa25aa5d5be10e0487
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-14 23:00:07 +0530

    codechef cpp

---
+++ chefdonatio0n.cpp
@@ -0,0 +1,20 @@
+// Chef and Donation
+// In a certain month, Chef earned Y>X.
+// Since they want to end up with exactly the same amount, they decide to donate the difference between their income to a charity.
+
+// Find the amount they donate in the month.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        cout<<y-x<<endl;
+    }
+
+}
