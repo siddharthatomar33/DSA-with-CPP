95f3a26382288698879bc6b9fbe9c166636916b9
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-15 20:10:30 +0530

    codechef cpp

---
+++ AirQualityIndex.cpp
@@ -0,0 +1,17 @@
+// Air Quality Index
+// In the light of G-20 summit, government has decided to keep the average air quality index (AQI) strictly below 100 On some random day, Chef measures the AQI and found the value to be X.Find whether the government was able to keep the AQI within limits.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin>>x;
+    
+    if(x<100){
+        cout<<"yes"<<endl;
+    } else {
+        cout<<"no"<<endl;
+    }
+}
