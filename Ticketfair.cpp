8afa346e37e7441e321d93f906c1b54971c79d31
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-15 15:21:31 +0530

    codechef cpp

---
+++ Ticketfair.cpp
@@ -0,0 +1,24 @@
+// Passes for Fair
+// There is a fair going on in Chefland. Chef wants to visit the fair along with his 
+// N friends. Chef manages to collect K passes for the fair. Will Chef be able to enter the fair with all his N friends?
+// A person can enter the fair using one pass, and each pass can be used by only one person.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    if(n>=k){
+	    cout<<"no"<<endl;
+	    }
+	    else {
+	        cout<<"yes"<<endl;
+	    }
+	}
+
+}
