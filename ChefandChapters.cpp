f7031d865b39a1b630c2801067890a6487564152
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-16 10:16:13 +0530

    codechef cpp

--- ChefandChapters.cpp
+++ ChefandChapters.cpp
@@ -1,6 +1,5 @@
 // Chef and Chapters
-// This semester, Chef took X courses. Each course has Y units and each unit has Z chapters in it.
-// Find the total number of chapters Chef has to study this semester.
+// This semester, Chef took X courses. Each course has Y units and each unit has Z chapters in it.  Find the total number of chapters Chef has to study this semester.
 
 
 
