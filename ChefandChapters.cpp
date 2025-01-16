15dcd5a884bdcf050436a6bd6fa3886e553b77bf
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-16 10:11:17 +0530

    codechef cpp

---
+++ ChefandChapters.cpp
@@ -0,0 +1,20 @@
+// Chef and Chapters
+// This semester, Chef took X courses. Each course has Y units and each unit has Z chapters in it.
+// Find the total number of chapters Chef has to study this semester.
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    cout<<x*y*z<<endl;
+	}
+
+}
