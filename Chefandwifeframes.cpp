8a1439e3f5590a2f984d24f066e89f80f1ef120a
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-16 14:27:51 +0530

    codechef cpp

---
+++ Chefandwifeframes.cpp
@@ -0,0 +1,16 @@
+// Chef and Wire Frames
+// Chef has a rectangular plate of. He wants to make a wireframe around the plate. The wireframe costs X rupees per cm.Determine the cost Chef needs to incur to buy the wireframe.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,m,x;
+	    cin>>n>>m>>x;
+	     cout<<(2*(n+m))*x<<endl;
+	}
+
+}
