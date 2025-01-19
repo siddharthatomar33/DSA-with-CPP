5f281b3a48d2668797818a06aa51bae87153fc0c
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-19 11:26:12 +0530

    codechef cpp

---
+++ temperaturechef.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int c;
+	    cin>>c;
+	    if(c<=20){
+	        cout<<"COLD"<<endl;
+	    }
+	    else {
+	        cout<<"HOT"<<endl;
+	    }
+	}
+
+}
