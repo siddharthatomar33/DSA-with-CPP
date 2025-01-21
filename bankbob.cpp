487153ee1ac6f188880ea2a6a322f1aeab978fef
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-21 11:23:17 +0530

    codechef cpp

---
+++ bankbob.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int w,x,y,z;
+	    cin>>w>>x>>y>>z;
+	    
+	    cout<<(w+(x*z)-(y*z))<<endl;
+	}
+
+}
