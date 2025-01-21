3b60583cecaab720100908821551f3a82938304d
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-21 10:50:00 +0530

    codechef cpp

---
+++ increaseiq.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	 if((x+7)>170){
+	     cout<<"yes"<<endl;
+	 }
+	 else{
+	     cout<<"no"<<endl;
+	 }
+
+}
