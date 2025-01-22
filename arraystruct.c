5dc4c9c75f3fa73ffba4a48ee7782cbc686ee2da
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-22 15:09:52 +0530

    codechef cpp

--- arraystruct.c
+++ arraystruct.c
@@ -13,12 +13,12 @@ strcpy(emp[1].name,"ram");
 strcpy(emp[2].name,"sid");
 strcpy(emp[3].name,"nara");
 strcpy(emp[4].name,"kri");
-
+//multi id's of employee input
 for(int i=0;i<5;i++){
     emp[i].id=i+1;
 }
 printf("\n employee details: \n");
-
+//printing multiple output
 for(int i=0;i<5;i++){
     printf("employee id: %d- employee name: %s\n",emp[i].id,emp[i].name);
 }
