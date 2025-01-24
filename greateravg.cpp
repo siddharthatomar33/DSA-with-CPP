c83c5da9fb5620fd4f21aabd3fe1075964627e4f
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-24 12:48:59 +0530

    codechef cpp

---
+++ greateravg.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+   {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    if(a+b>2*c){
+	    cout<<"yes"<<endl;
+	    }
+	    else{
+	    cout<<"no"<<endl;
+	    }
+	}
+}
---
+++ janmasnhassingment.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(10-n>=3){
+	        cout<<"yes"<<endl;
+	    }
+	    else{
+	        cout<<"no"<<endl;
+	    }
+	}
+
+}
