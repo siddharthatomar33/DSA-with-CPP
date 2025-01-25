c79e5209c01679ec2cf02ceb167747ef74d4e6e6
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-25 12:08:27 +0530

    c program

---
+++ filehandinput.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    FILE *fp1;
+    char c;
+    printf("data input \n");
+
+    fp1=fopen("FILE","w");
+    while((c=getchar())!=EOF)
+    putc(c,fp1);
+    fclose(fp1);
+    printf("data input\n");
+    fp1=fopen("output","r");
+    while((c=getchar())!=EOF)
+    printf("%c",c);
+    fclose(fp1);
+    return 0;
+}
