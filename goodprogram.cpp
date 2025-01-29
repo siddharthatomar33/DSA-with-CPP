9e6846c8508e0c79a7b8e315331b22033f7eeecd
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-01-29 11:20:02 +0530

    cpp program

---
+++ goodprogram.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n;
+	    if(n%4==0){
+	        cout<<"good"<<endl;
+	    }
+	    else {
+	        cout<<"not good"<<endl;
+	    }
+	}
+
+}
