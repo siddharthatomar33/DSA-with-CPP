92c11ddecc3632d569c8fa05eb06e95aac781855
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-02-08 21:33:12 +0530

    cpp program

---
+++ sugarcane.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while (t--){
+	    int n,ti;
+	    cin>>n;
+	    ti=n*50;
+	    cout<<ti-((ti*20/100)+(ti*20/100)+(ti*30/100))<<endl;
+	}
+
+}
