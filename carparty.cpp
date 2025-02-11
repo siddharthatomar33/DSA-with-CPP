dea7139986a9aa7e0c5afde881539ec03a76e315
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-02-11 22:55:45 +0530

    cpp program

---
+++ carparty.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n<=4){
+	        cout<<1<<endl;
+	    }
+	    else if(n%4==0){
+	        cout<<n/4<<endl;
+	    }
+	    else{
+	        cout<<(n/4)+1<<endl;
+	    }
+	}
+
+}
