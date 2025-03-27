9776d0735df70b0e222d83750bc0a29ebb63ef8c
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-03-28 00:06:07 +0530

    cpp program

--- array/linearsearchusingarrfunction.cpp
+++ array/linearsearchusingarrfunction.cpp
@@ -7,9 +7,27 @@ void printarray(int arr[],int size){
     }  
     cout<<endl;
 }
+bool linearsearch(int arr[],int size,int target){
+    for(int i=0;i<size;i++){
+        if(arr[i]==target){
+            //found
+            return true;
+        }
+    }
+    //not found
+    return 1;
+}
 int main(){
     int arr[5]={2,4,5,6,8};
     int size=5;
+    int target=10;
+
+    bool ans=linearsearch(arr,size,target);
+    if(ans==1){
+        cout<<"target found"<<endl;
+    }else{
+        cout<<"target not found"<<endl;
+    }
     //function call
-    printarray(arr,size);
+    //printarray(arr,size);
 }
---
+++ array/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+void printarray(int arr[],int size){  
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }  
+    cout<<endl;
+}
