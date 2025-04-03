034c9b92eb1bc0837ecae99aa35f48ac19f38bd2
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-04-03 09:55:25 +0530

    cpp program

---
+++ secondlargestndsecondsmallest.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(auto i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(auto i=0;i<n-1;i++){
+        for(auto j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                // int temp=arr[j];
+                // arr[j]=arr[j+1];
+                // arr[j+1]=temp;
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    cout<<arr[n-2]<<endl;
+    cout<<arr[1]<<endl;
+return 0;
+}
