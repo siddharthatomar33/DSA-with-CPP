c9d9e73f97f1d167fbf6abb1c1c223526ee08329
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-04-03 09:38:26 +0530

    cpp program

---
+++ evenorodd.cpp
@@ -0,0 +1,11 @@
+#include<iostream>;
+using namespace std;
+ int main(){
+    int n;
+    cin>>n;
+    arr[n];
+    for(auto i<0;i<n;i++){
+        cin>>arr[i];
+    }
+    for()
+ }
--- zerosinlast.cpp
+++ zerosinlast.cpp
@@ -10,7 +10,7 @@ int main(){
     }
     for(auto i=0;i<n-1;i++){
         for(auto j=0;j<n-1-i;j++){
-            if(arr[j]>arr[j+1]){
+            if(arr[j]<arr[j+1]){
                 // int temp=arr[j];
                 // arr[j]=arr[j+1];
                 // arr[j+1]=temp;
