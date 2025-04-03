59e626d41d053617caecbf91a6cf62c6ba24909e
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-04-03 09:20:35 +0530

    cpp program

---
+++ incrordecr.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(auto i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(auto i=0;i<n-1;i++){
+        for(auto j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    for(auto i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
