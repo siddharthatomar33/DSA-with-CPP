33ecf4ed63362e543551535b378ca0b38a178759
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-04-03 14:48:55 +0530

    cpp program

---
+++ thecoolerdilema798.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+while(t--){
+    int x,y;
+    cin>>x>>y;
+    if(y%x==0){
+    cout<<(y/x)-1<<endl;
+    }
+    else{
+        cout<<y/x<<endl;
+    }
+    
+}
+}
