ec67fc8b53277d4516a22c0ac0e83a18baed33ec
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-04-10 00:05:34 +0530

    cpp program

---
+++ searchnearlysorted.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int searchNearlysorted(int arr[],int n,int target){
+    int s=0,e=n-1,mid=s+(e-s)/2;
+    while(s<=e){
+        if(mid-1>0 && arr[mid-1]==target){ //pehele left may check hoga
+            return mid-1;
+        }
+        if(arr[mid]==target){
+            return mid;
+        }
+        if(arr[mid+1]==target){
+            return mid+1;
+        }
+        if(target>arr[mid]){
+            s=mid+2;
+        }else{
+            e=mid-2;
+        }
+    }mid=s+(e-s)/2;
+}
+return -1;
+
+int main(){
+    int arr[]={20,10,30,50,40,70,60};
+    int n=7;
+    int target=60;
+
+    int targetIndex=searchNearlysorted(arr,n,taregt);
+    if(targetIndex==-1){
+        cout<<"elememt not there"<<endl;
+        else
+        cout<<targetIndex<<endl;
+    }
+}
