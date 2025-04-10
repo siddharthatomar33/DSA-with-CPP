5ea309ade56cc72e136f171b7f6c8dd28b16ead8
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-04-10 10:35:03 +0530

    cpp program

---
+++ lastoccurence.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int findLastoccurence(int arr[],int n,int target){
+int s=0; 
+int e=n-1;
+int mid=s+(e-s)/2;
+int ans=-1;
+
+while(s<=e){
+    if(arr[mid]==target){
+        ans=mid;
+        s=mid+1;
+        //go and search for another same occurence
+    }
+    else if(target>arr[mid]){
+        //right
+        s=mid+1;
+    }
+    else if(target<arr[mid]){
+        e=mid-1;
+    }
+    mid=s+(e-s)/2;
+}
+return ans;
+
+}
+int main(){
+    int arr[]={10,20,30,30,30,40,50,60,70};
+    int target=30;
+    int n=9;
+    int ansIndex=findLastoccurence(arr,n,target);
+    if(ansIndex ==-1){
+        cout<<"element not found"<<endl;
+        }
+     else{
+        cout<<"element found at "<<ansIndex<<endl;
+        }
+ return 0;
+}
