94a325f67dbce23018b02b12a2930e9233f79117
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-04-13 23:20:53 +0530

    cpp program

--- array/vector2D.cpp
+++ array/vector2D.cpp
@@ -9,7 +9,7 @@ int main(){
 vector<vector<int>>arr(5,vector<int>(10,0));
 //row size -> arr.size()
 for(auto i=0;i<arr.size();i++){
-    for(auto j=0;j,arr[i].size();j++){
+    for(auto j=0;j<arr[i].size();j++){
         cout<<arr[i][j]<<" ";
     }
     cout<<endl;
