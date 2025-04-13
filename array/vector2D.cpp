cb49c78fc8baa1f4cfe7325d1a423113b62dc17b
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-04-13 23:18:44 +0530

    cpp program

---
+++ array/vector2D.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<limits.h>
+#include<vector>
+using namespace std;  //dynamic array
+
+int main(){
+// int arr[5];
+// vector<int>v(5);
+vector<vector<int>>arr(5,vector<int>(10,0));
+//row size -> arr.size()
+for(auto i=0;i<arr.size();i++){
+    for(auto j=0;j,arr[i].size();j++){
+        cout<<arr[i][j]<<" ";
+    }
+    cout<<endl;
+}
+
+return 0;
+}
---
+++ array/vectorpractise.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<limits.h>
+#include<vector>
+using namespace std;  //dynamic array
+
+int main(){
+int arr[5];
+vector<int>v(5);
+
+for(auto i=0;i<v.size();i++){
+    cout<<v[i]<<" ";
+}cout<<endl;
+
+return 0;
+}
