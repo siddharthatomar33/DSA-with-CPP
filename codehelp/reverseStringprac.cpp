8ea50d78a5f63b1510bffa308f16ec86cb0308e8
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-04-16 23:51:18 +0530

    cpp program

---
+++ codehelp/reverseStringprac.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int findLength(char ch[],int size){
+    int index=0;//
+    while(ch[index]!='\0'){//jab tk index=0 nhi hai length badate raho
+        index++;
+    }
+    return index;
+}
+
+void reverseString(char ch(), int n){
+    int i=0;
+    int j=n-1;
+    while(i<=j){
+        swap(ch[i],ch[j]);
+        i++;
+        j--;
+    }
+}
+int main(){
+    char ch[100];
+    cin>>getline(ch,100);
+
+    cout<<"before"<<ch<<endl;
+
+    int len=findLength(ch,100);
+
+    reverseString(ch,len);
+    
+    cout<<"after"<<ch<<endl;
+}
