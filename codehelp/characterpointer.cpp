1d012ab3b9f3fed434f0bf09845e8e79fe9ced2f
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-04-22 19:59:29 +0530

    cpp program

---
+++ codehelp/characterpointer.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int a=10;
+    // cout<<a<<endl;
+    // cout<<&a<<endl;
+    // //pointer creation
+    // int* ptr=&a;
+    // //accessing value stored at address stored in ptr
+    // cout<<*  ptr<<endl;
+    
+    char ch[50]="love";
+    char* cptr=ch;
+    cout<<cptr<<endl;
+
+    return 0;
+}
