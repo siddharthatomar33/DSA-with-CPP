4c32dff822790998e77e8d77a80b23de53ba5925
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-04-22 20:16:19 +0530

    cpp program

---
+++ codehelp/pracchpointer.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int a=10;
+    // cout<<a<<endl;
+    // cout<<&a<<endl;
+    // //pointer creation
+    // int* ptr=&a;
+    // //accessing value stored at address stored in ptr
+    // cout<<*  ptr<<endl;
+
+    // char ch[50]="love";
+    // char* cptr=ch;
+    // cout<<cptr<<endl;
+
+    char ch[30]="statement";
+    char * cptr=&ch[0];
+    
+    cout<<cptr<<endl;
+
+    return 0;
+}
