296499995813cc58a1fae846fac51a7b3d1c1a70
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-04-23 21:53:03 +0530

    cpp program

---
+++ codehelp/pracpointer.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void solve(int* arr,int size){
+    cout<<sizeof(arr)<<endl;
+}
+int main(){
+    int arr[]={10,20,30,40,50};
+    int size=5;
+
+    solve(arr,size);
+    return 0;
+}
