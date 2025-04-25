b0b11871bc9ae196e3cb6d32b15157b1cd401133
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-04-25 11:35:52 +0530

    cpp program

---
+++ codehelp/recursivepower1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int pow(int n){
+    //base case
+    if(n==0){
+        return 1;
+    }
+    //recursive call
+    //pow(n)=2*pow(n-1);
+
+    //int recckaans=pow(n-1);
+    int finalans=2*pow(n-1);
+    return finalans;
+    //processing(optional) 
+
+}
+int main(){
+
+    int n;
+    cout<<"enter the value of n: ";
+    cin>>n;
+
+    int ans=pow(n);
+    cout<<"power of "<<n<<" is: "<<ans<<endl;
+    return 0;
+}
