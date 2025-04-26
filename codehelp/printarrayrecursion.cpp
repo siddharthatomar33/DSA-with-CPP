5fe8eea5314dca1e7ce84cb8c208775f186e6790
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-04-26 23:08:06 +0530

    cpp program

---
+++ codehelp/printarrayrecursion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void printarray(int arr[],int n, int i){
+    //base case
+    if(i==n){
+        //array se bahr aagye hoo
+        return;
+    }
+    //recursive relation
+    //ek case humko solve krna hai
+    cout<<arr[i]<<" ";
+    //recursion call
+    printarray(arr, n, i+1);
+}
+
+int main(){
+    int arr[]={10,20,30,40,50,60};
+    int n=6;
+    int i=0;
+    printarray(arr,n,i);
+}
