dbbcca465645010b04dec9336e928d25d633960f
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-05-03 10:51:25 +0530

    cpp program

--- codehelp/inheritanceprac.cpp
+++ codehelp/inheritanceprac.cpp
@@ -29,7 +29,7 @@ class car:public vehicle{
 public:
     int noofdoors;
     string transmissiontype;
-
+public:
 car(string _name,string _model,int _nooftyres,int _noofdoors,string _transmissiontype):vehicle(_name,_model,_nooftyres){
     cout<<"i m inside car ctor"<<endl;
     this->noofdoors=_noofdoors;
@@ -40,12 +40,31 @@ car(string _name,string _model,int _nooftyres,int _noofdoors,string _transmissio
         cout<<"ac has started of"<<name<<endl;
     }
 };
+class motorcycle:public vehicle{
+protected:
+    string handstyle;
+    string suspension;
+public:
+motorcycle(string _name,string _model,int _nooftyres,string handstyle,string suspension):vehicle(_name,_model,_nooftyres){
+    cout<<"motorcycle ctor called"<<endl;    
+    this->handstyle=handstyle;
+        this->suspension=suspension;
+    }
+    void wheelie(){
+        cout<<"wheelie done by bike"<<name<<endl;
+    }
+};
 
   int main(){
     car A ("maruti  800","LXI",4,4,"manual");
     A.startengine();
     A.startac();
     A.stopengine();
+
+    motorcycle M("BMW","X1",2,"U","Hard");
+    M.startengine();
+    M.wheelie();
+    M.stopengine();
 
     return 0;
   }
