574d4ed6f4ce5e3769be2ff65fee0d62ca0e5a24
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-06-05 11:56:40 +0530

    cpp program

--- codehelp/doblyqueue.cpp
+++ codehelp/doblyqueue.cpp
@@ -2,7 +2,7 @@
 #include<queue>
 using namespace std;
 
-class deque{
+class Deque{
     private:
     int* arr;
     int size;
@@ -10,14 +10,28 @@ class deque{
     int rear;
 
     public:
-           deque(int size){
+           Deque(int size){
             arr=new int[size];
             front=-1;
             rear=-1;
            }
 
     void pushfront(int val){
-       
+       if(front==0){
+        //means we dont have space in left of the queue
+        cout<<"overflow"<<endl;
+       }
+       else if(front==-1 && rear==-1){
+        //empty
+        front++;
+        rear++;
+        arr[front]=val;
+       }
+       else{
+        //normal case
+        front--;
+        arr[front]=val;
+       }
     }
     void pushback(int val){
         //3 cases
@@ -47,31 +61,63 @@ class deque{
        }
        else{
         //normal flow
-        arr[front]
+        arr[front]=-1;
+        front++;
        }
     }
     void popback(){
-
+       if(front==-1 && rear=-1){ //empty ,no element to pop
+        cout<<"underflow"<<endl;
+       }
+       else if(front==rear){
+        //single element
+        arr[rear]=-1;
+        front--;
+        rear--;
+       }
+       else{
+        //normal case
+        arr[rear]=-1;
+        rear--;
+       }
+    }
+    void print(){
+        for(int i=0;i<size;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
     }
 
 };
 
 int main(){
-    //creation
-    deque<int>dq;
+    Deque dq(5);
+    
+    dq.pushfront(10);
+    dq.print();
+    dq.pushfront(20);
+    dq.print();
+    dq.pushback(15);
+    dq.print();
+    dq.pushback(35);
+    dq.print();
+    dq.pushfront(100);
+    dq.print();
+    dq.popback();
+    dq.print();
+    dq.popback();
+    dq.print();
 
     //insertion
-    dq.push_front(10);
-    dq.push_front(30);
-    dq.push_front(50);
-    dq.push_back(70);
+    // dq.push_front(10);
+    // dq.push_front(30);
+    // dq.push_front(50);
+    // dq.push_back(70);
 
-    //removal
-    dq.pop_front();
-    cout<<dq.front()<<endl;
-    dq.pop_back();
-    cout<<dq.back()<<endl;
-
-
+    // //removal
+    // dq.pop_front();
+    // cout<<dq.front()<<endl;
+    // dq.pop_back();
+    // cout<<dq.back()<<endl;
     return 0;
 }
--- codehelp/tempCodeRunnerFile.cpp
+++ codehelp/tempCodeRunnerFile.cpp
@@ -1,3 +1,1 @@
-s.push(10);
-    // s.push(20);
-    // s.push(30);
+arr[rear]=-1;
