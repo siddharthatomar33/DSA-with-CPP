dd6ca36e8fc356445b9069a007ff2287bdef3e3c
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-06-13 23:44:22 +0530

    cpp program

---
+++ codehelp/circularqueuepractise.cpp
@@ -0,0 +1,12 @@
+ #include<iostream>
+ #include<queue>
+ using namespace std;
+
+ class circularqueue{
+    public:
+    int * arr;
+    int n;
+    int front;
+    int rear;
+ }
+  circularqueue(int )
