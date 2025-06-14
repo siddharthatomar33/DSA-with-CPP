dec770540ddf85b80180bd332a59676b60069ee2
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-06-14 23:23:57 +0530

    cpp program

--- codehelp/Decircularqueueprac.cpp
+++ codehelp/Decircularqueueprac.cpp
@@ -23,7 +23,47 @@
     if((front==0 && rear==n-1)||(rear=front-1)){
         cout<<"overflow"<<endl;
     }
-    else if(front==-1 && rear==-1)
+    else if(front==-1 && rear==-1){//first element
+     rear++;
+     front++;
+     arr[rear]=val;
+    }
+    else if(front==0 && rear!=n-1){
+        //circular nature
+        front--;
+        arr[front]=val;
+    }
+    else{
+        //normaal case
+        front--;
+        arr[front]=val;
+    }
+   }
 
-  }
+   void pushback(int val){
+    //4 cases
+    //overflow
+    if((front==-1 && rear==n-1)|| (rear=front-1)){
+       cout<<"overflow"<<endl;
+    }
+    //first element
+    else if(front==-1 && rear==-1){
+        front++;
+        rear++;
+        arr[rear]=val;
+    }
+    //cicular nature
+    else if(rear==n-1 && front!=0){
+        rear=0;
+        arr[rear]=val;
+    }
+    else{
+        //normal case
+        rear++;
+        arr[rear]=val;
+    }
+   }
+   void popfront(int val){
+    
+   }
 };
