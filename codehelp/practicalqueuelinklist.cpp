9d3a8d15c437696b202edf83054646b9c298144f
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-07-30 11:21:31 +0530

    cpp program

---
+++ codehelp/practicalqueuelinklist.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+// Function to merge two halves
+void merge(int arr[], int left, int mid, int right) {
+    // Sizes of the subarrays
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    // Temporary arrays
+    int* L = new int[n1];
+    int* R = new int[n2];
+
+    // Copy data to temp arrays
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    // Merge the temp arrays
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    // Copy remaining elements
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+
+    // Free memory
+    delete[] L;
+    delete[] R;
+}
+
+// Function to apply merge sort
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        // Recursively sort halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+
+// Main function
+int main() {
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original Array:\n";
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "\nSorted Array using Merge Sort:\n";
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+
+    return 0;
+}
